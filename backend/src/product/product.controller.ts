7de1095d29e2832455b5d9667a05c0c3836234d9
Sifytul <sifytul09@gmail.com>
2023-10-17 10:21:21 +0600

    🎨 scaff: added related route of products

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,34 +1,42 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ProductService } from './product.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Public } from 'src/auth/decorators/public.decorator';
+import { Role, Roles } from 'src/auth/decorators/roles.decorator';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductService } from './product.service';
 
-@Controller('product')
+@Controller()
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
-  }
+  @Public()
+  @Get('products')
+  async findAllProducts() {}
 
-  @Get()
-  findAll() {
-    return this.productService.findAll();
-  }
+  @Public()
+  @Get('products/:id')
+  findOneProductById(@Param('id') id: string) {}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
-  }
+  @Roles(Role.ADMIN)
+  @Post('admin/products/new')
+  createProduct(@Body() createProductDto: CreateProductDto) {}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
-  }
+  @Roles(Role.ADMIN)
+  @Patch('admin/products/:id')
+  updateProductById(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
-  }
+  @Roles(Role.ADMIN)
+  @Delete('admin/products/:id')
+  deleteProductById(@Param('id') id: string) {}
 }
