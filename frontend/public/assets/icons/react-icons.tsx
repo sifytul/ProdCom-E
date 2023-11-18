edf088bbdf325f8c7d9b68f755b6e92d29281d6f
Sifytul <sifytul09@gmail.com>
2023-11-18 15:10:30 +0600

    🛠️ refactor: refactored the react icons

---
+++ frontend/public/assets/icons/react-icons.tsx
@@ -0,0 +1,28 @@
+import {
+  BsEyeFill,
+  BsEyeSlashFill,
+  BsFillPersonFill,
+  BsPersonCircle,
+} from "react-icons/bs";
+import { CgKey } from "react-icons/cg";
+import { FaEdit, FaHome } from "react-icons/fa";
+import { FiEdit, FiMenu } from "react-icons/fi";
+import { HiXCircle } from "react-icons/hi";
+import { MdPersonSearch, MdPlayArrow } from "react-icons/md";
+import { RxCross1 } from "react-icons/rx";
+
+export {
+  BsEyeFill,
+  BsEyeSlashFill,
+  BsFillPersonFill,
+  BsPersonCircle,
+  CgKey,
+  FaEdit,
+  FaHome,
+  FiEdit,
+  FiMenu,
+  HiXCircle,
+  MdPersonSearch,
+  MdPlayArrow,
+  RxCross1,
+};
