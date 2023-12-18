86a3e6fc7da2af975262e8c6792a35f81ca8055e
sifytul <sifytul09@gmail.com>
2023-12-19 00:58:09 +0600

    🐘 db: add insert query to add admin - migrationfile

---
+++ backend/src/migrations/1702907760963-admin.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Admin1702907760963 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `INSERT INTO "user"("name", "email", "password", "role") VALUES ('admin','admin@email.com','$2b$10$bJCzRoSm7GhIohTYLo1NTufIBkCvoujzbtTivTDjCKHwwxJXgZT3.', 'admin')`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
